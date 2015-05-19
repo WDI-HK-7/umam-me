e359a6964fe96f356dd18014f78c0de3e3d0b5ea
“mia” <mia_dacamara@Mias-MacBook-Pro.local>
2015-05-19 18:22:45 +0800

    Generate controller

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,2 +1,11 @@
 class DishesController < ApplicationController
+	def create
+		@dish = Dish.create(dish_params)
+	end
+
+	private
+
+	def dish_params
+		params.require(:dish).permit(:name, :content, :restaurant_id)
+	end
 end
