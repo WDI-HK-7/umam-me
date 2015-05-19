1fa95a8eb84afc0f933dae0538d75cac3f6ad86b
“mia” <mia_dacamara@Mias-MacBook-Pro.local>
2015-05-19 14:37:31 +0800

    Created Dishes Routes, Model and Migrations

---
+++ umamme/app/models/dish.rb
@@ -0,0 +1,8 @@
+class Dish < ActiveRecord::Base
+	belongs_to :user
+	belongs_to :restaurants
+	has_many :comments
+
+	has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }
+	validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/  
+end
