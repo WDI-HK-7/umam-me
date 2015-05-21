8e483374be1ed8ef7a1ef2a8519dfbba8d674389
“mia” <mia_dacamara@Mias-MacBook-Pro.local>
2015-05-21 14:53:56 +0800

    fixed indentation in migration files

--- db/migrate/20150519082221_create_comments.rb
+++ db/migrate/20150519082221_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-    	t.integer :dish_id
+      t.integer :dish_id
       t.integer :user_id
       t.text :content
       t.belongs_to :dish, index: true, foreign_key: true
