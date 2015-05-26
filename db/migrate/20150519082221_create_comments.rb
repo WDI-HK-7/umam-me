b1bff506169229d689de5a7647cea5ce808741a1
“mia” <mia_dacamara@Mias-MacBook-Pro.local>
2015-05-26 15:28:45 +0800

    deploy to heroku ammendments to migration

--- db/migrate/20150519082221_create_comments.rb
+++ db/migrate/20150519082221_create_comments.rb
@@ -1,8 +1,6 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.integer :dish_id
-      t.integer :user_id
       t.text :content
       t.belongs_to :dish, index: true, foreign_key: true
 
